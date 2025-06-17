afa18daa499d855521e95dff93490782e549217b
BanuKarakaya <nurbanukarakaya03@gmail.com>
2025-06-17 21:49:12 +0300

    initial commit

---
+++ WorldLens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldLens
+//
+//  Created by Banu on 17.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WorldLensTests/WorldLensTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorldLensTests.swift
+//  WorldLensTests
+//
+//  Created by Banu on 17.06.2025.
+//
+
+import Testing
+@testable import WorldLens
+
+struct WorldLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
